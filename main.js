c7f533a477121378978f68e7ba27c6f16087f6d1
vipin018 <vipinpathak018@gmail.com>
2025-05-07 23:05:59 +0530

    created back button

--- main.js
+++ main.js
@@ -1,13 +1,24 @@
 let allElems = document.querySelectorAll(".elem");
 let allFullElems = document.querySelectorAll(".fullElem");
 let allElemsContainer = document.querySelector(".allElems");
+let allFullElemsBackBtn = document.querySelectorAll(".fullElem .back");
 
-allElems.forEach(function(elem) {
-  elem.addEventListener("click", function() {
-    // Hide all .fullElem sections first
-    allFullElems.forEach(section => section.style.display = "none");
-
+allElems.forEach(function (elem, index) {
+  elem.addEventListener("click", function () {
+    // Hide grid cards
     allElemsContainer.style.display = "none";
-    allFullElems[elem.id].style.display = "block";
+
+    // Show the clicked fullElem section
+    allFullElems[index].style.display = "flex";
+  });
+});
+
+allFullElemsBackBtn.forEach(function (btn, index) {
+  btn.addEventListener("click", function () {
+    // Hide current fullElem section
+    allFullElems[index].style.display = "none";
+
+    // Show the cards container again
+    allElemsContainer.style.display = "flex";
   });
 });
